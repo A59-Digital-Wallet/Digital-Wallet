1ead9aca18bb51eb6db25569bd575e4eca4be423
Rosen <rosen.damyanov7@gmail.com>
2024-09-01 15:40:34 +0300

    Added more API tests

---
+++ Wallet.API.Tests/AdminControllerTests/AdminControllerTests.cs
@@ -0,0 +1,281 @@
+﻿using Digital_Wallet.Controllers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Wallet.Common.Helpers;
+using Wallet.Data.Models;
+using Wallet.DTO.Response;
+using Wallet.Services.Contracts;
+using Wallet.Services.Factory;
+
+namespace Wallet.API.Tests.AdminControllerTests
+{
+    [TestClass]
+    public class AdminControllerTests
+    {
+
+        private Mock<IUserService> _mockUserService;
+        private Mock<UserManager<AppUser>> _mockUserManager;
+        private Mock<IOverdraftSettingsService> _mockOverdraftSettingsService;
+        private AdminController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockUserService = new Mock<IUserService>();
+            _mockOverdraftSettingsService = new Mock<IOverdraftSettingsService>();
+            var mockUserStore = new Mock<IUserStore<AppUser>>();
+            _mockUserManager = new Mock<UserManager<AppUser>>(mockUserStore.Object, null, null, null, null, null, null, null, null);
+
+            _controller = new AdminController(_mockUserService.Object, _mockUserManager.Object, _mockOverdraftSettingsService.Object);
+        }
+
+        [TestMethod]
+        public async Task GetUsers_ReturnsOkResult_WithUserList()
+        {
+            // Arrange
+            var pagedResult = new PagedResult<UserWithRolesDto>
+            {
+                Items = new List<UserWithRolesDto> // Corrected from 'Results' to 'Items'
+                {
+                    new UserWithRolesDto { User = new AppUser { Id = "1", UserName = "testuser1" }, Roles = new List<string> { "Admin" } },
+                    new UserWithRolesDto { User = new AppUser { Id = "2", UserName = "testuser2" }, Roles = new List<string> { "User" } }
+                },
+                TotalCount = 2
+            };
+
+            _mockUserService.Setup(service => service.SearchUsersAsync(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(pagedResult); // Correct type used in ReturnsAsync
+
+            // Act
+            var result = await _controller.GetUsers(null);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(pagedResult, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task GetUserById_ReturnsOkResult_WithUser()
+        {
+            // Arrange
+            var user = new AppUser { Id = "1", UserName = "testuser1" };
+            _mockUserService.Setup(service => service.GetUserByIdAsync(It.IsAny<string>()))
+                .ReturnsAsync(user);
+
+            // Act
+            var result = await _controller.GetUserById("1");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(user, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task GetUserById_ReturnsNotFound_WhenUserNotFound()
+        {
+            // Arrange
+            _mockUserService.Setup(service => service.GetUserByIdAsync(It.IsAny<string>()))
+                .ReturnsAsync((AppUser)null);
+
+            // Act
+            var result = await _controller.GetUserById("1");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+            var notFoundResult = result as NotFoundObjectResult;
+            Assert.IsNotNull(notFoundResult);
+            Assert.AreEqual(Messages.UserNotFound, notFoundResult.Value);
+        }
+
+        [TestMethod]
+        public async Task ManageRole_ReturnsOkResult_WhenActionSuccessful()
+        {
+            // Arrange
+            var identityResult = IdentityResult.Success;
+            _mockUserService.Setup(service => service.ManageRoleAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(identityResult);
+
+            // Act
+            var result = await _controller.ManageRole("userId", "add");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(string.Format(Messages.Controller.ActionSuccessful, "add"), okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task ManageRole_ReturnsBadRequest_WhenActionFails()
+        {
+            // Arrange
+            var identityResult = IdentityResult.Failed(new IdentityError { Description = "Failed to add role." });
+            _mockUserService.Setup(service => service.ManageRoleAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(identityResult);
+
+            // Act
+            var result = await _controller.ManageRole("userId", "add");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+            var badRequestResult = result as BadRequestObjectResult;
+            Assert.IsNotNull(badRequestResult);
+            Assert.AreEqual($"Failed to perform 'add' action on the user. Errors: Failed to add role.", badRequestResult.Value);
+        }
+
+
+        [TestMethod]
+        public async Task ManageRole_ReturnsNotFound_WhenKeyNotFoundExceptionThrown()
+        {
+            // Arrange
+            _mockUserService.Setup(service => service.ManageRoleAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ThrowsAsync(new KeyNotFoundException("User not found"));
+
+            // Act
+            var result = await _controller.ManageRole("invalidUserId", "add");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+            var notFoundResult = result as NotFoundObjectResult;
+            Assert.IsNotNull(notFoundResult);
+            Assert.AreEqual("User not found", notFoundResult.Value);
+        }
+
+        [TestMethod]
+        public async Task ManageRole_ReturnsBadRequest_WhenArgumentExceptionThrown()
+        {
+            // Arrange
+            _mockUserService.Setup(service => service.ManageRoleAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ThrowsAsync(new ArgumentException("Invalid action"));
+
+            // Act
+            var result = await _controller.ManageRole("userId", "invalidAction");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+            var badRequestResult = result as BadRequestObjectResult;
+            Assert.IsNotNull(badRequestResult);
+            Assert.AreEqual("Invalid action", badRequestResult.Value);
+        }
+
+        [TestMethod]
+        public async Task ManageRole_ReturnsInternalServerError_WhenExceptionThrown()
+        {
+            // Arrange
+            _mockUserService.Setup(service => service.ManageRoleAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ThrowsAsync(new Exception("Unexpected error"));
+
+            // Act
+            var result = await _controller.ManageRole("userId", "add");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ObjectResult));
+            var objectResult = result as ObjectResult;
+            Assert.IsNotNull(objectResult);
+            Assert.AreEqual(500, objectResult.StatusCode);
+            Assert.AreEqual(Messages.OperationFailed, objectResult.Value);
+        }
+
+        [TestMethod]
+        public async Task SetInterestRate_ReturnsOkResult_WhenSuccessful()
+        {
+            // Arrange
+            _mockOverdraftSettingsService.Setup(service => service.SetInterestRateAsync(It.IsAny<decimal>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.SetInterestRate(0.05m);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(string.Format(Messages.Controller.InterestRateSuccessful, 0.05m), okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task SetInterestRate_ReturnsBadRequest_WhenFailed()
+        {
+            // Arrange
+            _mockOverdraftSettingsService.Setup(service => service.SetInterestRateAsync(It.IsAny<decimal>()))
+                .ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.SetInterestRate(0.05m);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+
+        [TestMethod]
+        public async Task SetOverdraftLimit_ReturnsOkResult_WhenSuccessful()
+        {
+            // Arrange
+            _mockOverdraftSettingsService.Setup(service => service.SetOverdraftLimitAsync(It.IsAny<decimal>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.SetOverdraftLimit(1000);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(string.Format(Messages.Controller.OverdraftLimitSuccessful, 1000), okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task SetOverdraftLimit_ReturnsBadRequest_WhenFailed()
+        {
+            // Arrange
+            _mockOverdraftSettingsService.Setup(service => service.SetOverdraftLimitAsync(It.IsAny<decimal>()))
+                .ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.SetOverdraftLimit(1000);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+
+        [TestMethod]
+        public async Task SetConsecutiveNegativeMonths_ReturnsOkResult_WhenSuccessful()
+        {
+            // Arrange
+            _mockOverdraftSettingsService.Setup(service => service.SetConsecutiveNegativeMonthsAsync(It.IsAny<int>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.SetConsecutiveNegativeMonths(3);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(string.Format(Messages.Controller.NegativeMonthsSuccessful, 3), okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task SetConsecutiveNegativeMonths_ReturnsBadRequest_WhenFailed()
+        {
+            // Arrange
+            _mockOverdraftSettingsService.Setup(service => service.SetConsecutiveNegativeMonthsAsync(It.IsAny<int>()))
+                .ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.SetConsecutiveNegativeMonths(3);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+            var badRequestResult = result as BadRequestObjectResult;
+            Assert.IsNotNull(badRequestResult);
+            Assert.AreEqual(Messages.Controller.NegativeMonthsFailed, badRequestResult.Value);
+        }
+
+    }
+}
---
+++ Wallet.API.Tests/CategoryControllerTests/CategoryControllerTests.cs
@@ -0,0 +1,284 @@
+﻿using Digital_Wallet.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Security.Claims;
+using Wallet.Common.Exceptions;
+using Wallet.Common.Helpers;
+using Wallet.DTO.Request;
+using Wallet.DTO.Response;
+using Wallet.Services.Contracts;
+
+namespace Wallet.API.Tests.CategoryControllerTests
+{
+    [TestClass]
+    public class CategoryControllerTests
+    {
+        private Mock<ICategoryService> _mockCategoryService;
+        private CategoryController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockCategoryService = new Mock<ICategoryService>();
+
+            _controller = new CategoryController(_mockCategoryService.Object);
+
+            // Set up a mock user with a specific user ID
+            var userId = "test-user-id";
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.UserData, userId)
+            };
+
+            var mockIdentity = new ClaimsIdentity(claims);
+            var mockPrincipal = new ClaimsPrincipal(mockIdentity);
+
+            // Set up the controller context
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = mockPrincipal }
+            };
+        }
+
+        [TestMethod]
+        public async Task GetUserCategories_ReturnsOkResult_WithCategories()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            var categories = new List<CategoryResponseDTO>
+            {
+                new CategoryResponseDTO { Id = 1, Name = "Category 1" },
+                new CategoryResponseDTO { Id = 2, Name = "Category 2" }
+            };
+
+            _mockCategoryService.Setup(service => service.GetUserCategoriesAsync(userId, It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(categories);
+
+            // Act
+            var result = await _controller.GetUserCategories();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            CollectionAssert.AreEqual(categories, (List<CategoryResponseDTO>)okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task GetUserCategories_ReturnsNotFound_WhenEntityNotFoundExceptionThrown()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            _mockCategoryService.Setup(service => service.GetUserCategoriesAsync(userId, It.IsAny<int>(), It.IsAny<int>()))
+                .ThrowsAsync(new EntityNotFoundException("Categories not found"));
+
+            // Act
+            var result = await _controller.GetUserCategories();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+            var notFoundResult = result as NotFoundObjectResult;
+            Assert.AreEqual("Categories not found", notFoundResult.Value);
+        }
+
+        [TestMethod]
+        public async Task AddCategory_ReturnsOkResult_WhenSuccessful()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            var categoryRequest = new CategoryRequestDTO { Name = "New Category" };
+
+            _mockCategoryService.Setup(service => service.AddCategoryAsync(userId, categoryRequest))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.AddCategory(categoryRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+
+            var messageProperty = okResult.Value.GetType().GetProperty("message");
+            Assert.IsNotNull(messageProperty);
+            Assert.AreEqual(Messages.Controller.CategoryAddedSuccessful, messageProperty.GetValue(okResult.Value));
+        }
+
+        [TestMethod]
+        public async Task AddCategory_ReturnsBadRequest_WhenArgumentNullExceptionThrown()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            var categoryRequest = new CategoryRequestDTO { Name = null }; // Invalid input
+
+            _mockCategoryService.Setup(service => service.AddCategoryAsync(userId, categoryRequest))
+                .ThrowsAsync(new ArgumentNullException(nameof(categoryRequest), Messages.Service.CategoryNameCannotBeEmpty));
+
+            // Act
+            var result = await _controller.AddCategory(categoryRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+            var badRequestResult = result as BadRequestObjectResult;
+            var actualMessage = ExtractActualMessageFromException(badRequestResult.Value.ToString());
+
+            Assert.AreEqual(Messages.Service.CategoryNameCannotBeEmpty, actualMessage);
+        }
+
+        [TestMethod]
+        public async Task UpdateCategory_ReturnsOkResult_WhenSuccessful()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            var categoryId = 1;
+            var categoryRequest = new CategoryRequestDTO { Name = "Updated Category" };
+            var updatedCategory = new CategoryResponseDTO { Id = 1, Name = "Updated Category" };
+
+            _mockCategoryService.Setup(service => service.UpdateCategoryAsync(userId, categoryId, categoryRequest))
+                .ReturnsAsync(updatedCategory);
+
+            // Act
+            var result = await _controller.UpdateCategory(categoryId, categoryRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(updatedCategory, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task UpdateCategory_ReturnsBadRequest_WhenArgumentNullExceptionThrown()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            var categoryId = 1;
+            var categoryRequest = new CategoryRequestDTO { Name = null }; // Invalid input
+
+            _mockCategoryService.Setup(service => service.UpdateCategoryAsync(userId, categoryId, categoryRequest))
+                .ThrowsAsync(new ArgumentNullException(nameof(categoryRequest), Messages.Service.CategoryNameCannotBeEmpty)); // Use custom message
+
+            // Act
+            var result = await _controller.UpdateCategory(categoryId, categoryRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+            var badRequestResult = result as BadRequestObjectResult;
+
+            var actualMessage = ExtractActualMessageFromException(badRequestResult.Value.ToString());
+            Assert.AreEqual(Messages.Service.CategoryNameCannotBeEmpty, actualMessage);
+        }
+
+
+
+        [TestMethod]
+        public async Task UpdateCategory_ReturnsNotFound_WhenEntityNotFoundExceptionThrown()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            var categoryId = 1;
+            var categoryRequest = new CategoryRequestDTO { Name = "Updated Category" };
+
+            _mockCategoryService.Setup(service => service.UpdateCategoryAsync(userId, categoryId, categoryRequest))
+                .ThrowsAsync(new EntityNotFoundException("Category not found"));
+
+            // Act
+            var result = await _controller.UpdateCategory(categoryId, categoryRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+            var notFoundResult = result as NotFoundObjectResult;
+            Assert.AreEqual("Category not found", notFoundResult.Value);
+        }
+
+        [TestMethod]
+        public async Task UpdateCategory_ReturnsForbid_WhenAuthorizationExceptionThrown()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            var categoryId = 1;
+            var categoryRequest = new CategoryRequestDTO { Name = "Updated Category" };
+
+            _mockCategoryService.Setup(service => service.UpdateCategoryAsync(userId, categoryId, categoryRequest))
+                .ThrowsAsync(new AuthorizationException("Not authorized"));
+
+            // Act
+            var result = await _controller.UpdateCategory(categoryId, categoryRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ForbidResult));
+        }
+
+        [TestMethod]
+        public async Task DeleteCategoryAsync_ReturnsOkResult_WhenSuccessful()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            var categoryId = 1;
+
+            // Corrected mock setup
+            _mockCategoryService.Setup(service => service.DeleteCategoryAsync(userId, categoryId)).Returns(Task.CompletedTask);  // Ensure ReturnsAsync is used correctly with the expected return type
+
+            // Act
+            var result = await _controller.DeleteCategoryAsync(categoryId);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+
+            // Correctly access the 'message' property from the anonymous object
+            var messageProperty = okResult.Value.GetType().GetProperty("message");
+            Assert.IsNotNull(messageProperty);
+            Assert.AreEqual(Messages.Controller.CategoryDeletedSuccessful, messageProperty.GetValue(okResult.Value));
+        }
+
+        [TestMethod]
+        public async Task DeleteCategoryAsync_ReturnsNotFound_WhenEntityNotFoundExceptionThrown()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            var categoryId = 1;
+
+            _mockCategoryService.Setup(service => service.DeleteCategoryAsync(userId, categoryId))
+                .ThrowsAsync(new EntityNotFoundException("Category not found"));
+
+            // Act
+            var result = await _controller.DeleteCategoryAsync(categoryId);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+            var notFoundResult = result as NotFoundObjectResult;
+            Assert.AreEqual("Category not found", notFoundResult.Value);
+        }
+
+        [TestMethod]
+        public async Task DeleteCategoryAsync_ReturnsForbid_WhenAuthorizationExceptionThrown()
+        {
+            // Arrange
+            var userId = "test-user-id";
+            var categoryId = 1;
+
+            _mockCategoryService.Setup(service => service.DeleteCategoryAsync(userId, categoryId))
+                .ThrowsAsync(new AuthorizationException("Not authorized"));
+
+            // Act
+            var result = await _controller.DeleteCategoryAsync(categoryId);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ForbidResult));
+        }
+
+        private string ExtractActualMessageFromException(string exceptionMessage)
+        {
+            // Extracts the actual message without the parameter part
+            var index = exceptionMessage.IndexOf(" (Parameter");
+            if (index > 0)
+            {
+                return exceptionMessage.Substring(0, index);
+            }
+            return exceptionMessage;
+        }
+    }
+}
